9b2d5e297bae5ab91fb5d863c19401c47f536795
ifmiss <185098535@qq.com>
2020-01-17 02:15:37 +0800

    Update template.js

--- utils/template.js
+++ utils/template.js
@@ -1,25 +1,34 @@
 const handlebars = require('handlebars')
 const Metalsmith = require('metalsmith')
-const fs = require('fs')
 const path = require('path')
+const minimatch = require("minimatch")
+const fs = require('fs')
+
+const {
+  existsSync,
+  removeFileOrDirSync
+} = require('./file')
+
+// 注册 equal 动作
+// handlebars.registerHelper('equal', function(v1, v2, options) {
+//   if(v1 === v2) {
+//     return options.fn(this);
+//   }
+//   return options.inverse(this);
+// });
 
 const Tpl = {
   initPackageJson: (projectInfo) => {
-    
-    try {
-      console.log('to initPackageJson')
-      const fileName = path.join(projectInfo.name, 'package.json')
-      console.log(fileName)
-      if (fs.existsSync(fileName)) {
-        const content = fs.readFileSync(fileName).toString()
-        const result = handlebars.compile(content)(projectInfo)
-        console.log('result', result)
-        // 重写覆盖
-        fs.writeFileSync(fileName, result)
-      }
-    } catch (err) {
-      console.log('err', err)
+    const packageObj = {
+      name: projectInfo.name,
+      description: projectInfo.description,
+      author: projectInfo.author
     }
+    const fileName = `${packageObj.name}/package.json`
+    const content = fs.readFileSync(fileName).toString()
+    const result = handlebars.compile(content)(packageObj)
+    // 重写覆盖
+    fs.writeFileSync(fileName, result)
   },
 
   /**
@@ -27,6 +36,9 @@ const Tpl = {
    */
   initFile: (projectInfo) => {
     return new Promise((resolve, reject) => {
+      // 初始化 package.json
+      Tpl.initPackageJson(projectInfo)
+
       const metalsmith = Metalsmith(process.cwd())
                       .metadata(projectInfo)
                       .clean(false)
@@ -37,23 +49,53 @@ const Tpl = {
         const meta = metalsmith.metadata();
         Object.keys(files).forEach(fileName => {
           const fileText = files[fileName].contents.toString()
-          if (fileName === 'package.json') {
-            console.log('------')
-            Tpl.initPackageJson(projectInfo)
-          } else {
+          if (fileName !== 'package.json') {
             files[fileName].contents = Buffer.from(handlebars.compile(fileText)(meta));
             // fs.writeFileSync(fileName, files[fileName].contents)
-            // console.log(files[fileName].contents.toString())
           }
         })
         // package_temp_content = handlebars.compile(fs.readFileSync(packjsonTemp).toString())(meta);
         done();
+
+        // 移除需要忽略的文档
+        // await Tpl.removeIgnoreFile(projectInfo, metalsmith)
       }).build(err => {
         // remove(src);
         err ? reject(err) : resolve(projectInfo);
+        // Tpl.removeIgnoreFile(projectInfo, metalsmith).build(err => {
+        //   // remove(src);
+        //   err ? reject(err) : resolve(projectInfo);
+        // })
       })
     })
+  },
+
+  removeIgnoreFile: (projectInfo, metalsmith) => {
+    const ignoreFile = path.join(projectInfo.name, 'templates.ignore')
+    if (existsSync(ignoreFile)) {
+      // 定义一个用于移除模板中被忽略文件的metalsmith插件
+      metalsmith.use((files, metalsmith, done) => {
+        const meta = metalsmith.metadata()
+        // 先对ignore文件进行渲染，然后按行切割ignore文件的内容，拿到被忽略清单
+        const ignores = handlebars.compile(fs.readFileSync(ignoreFile).toString())(meta)
+                                  .split('\n').filter(item => !!item.length)
+        Object.keys(files).forEach(fileName => {
+          // 移除被忽略的文件
+          console.log('fileName', fileName)
+          ignores.forEach(ignorePattern => {
+            console.log('ignorePattern', ignorePattern)
+            if (minimatch(fileName, ignorePattern)) {
+              // delete files[fileName]
+              console.log('---', fileName)
+              removeFileOrDirSync(path.join(projectInfo.name, fileName))
+            }
+          })
+        })
+        done()
+      })
+    }
   }
 }
 
 module.exports = Tpl
+
