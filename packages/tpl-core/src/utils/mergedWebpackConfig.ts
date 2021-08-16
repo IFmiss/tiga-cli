c60d33f7bf75da5894bbb109b32cb6271311502a
ifmiss <185098535@qq.com>
2021-08-16 23:30:59 +0800

    update: support chainWebpack

--- packages/tpl-core/src/utils/mergedWebpackConfig.ts
+++ packages/tpl-core/src/utils/mergedWebpackConfig.ts
@@ -1,16 +1,44 @@
+import Config from 'webpack-chain';
+
 import { workSpaceNodeModules } from '../index';
 import { TigaConfig } from '../types';
+
+const config = new Config();
 
 export default function mergedWebpackConfig(
   webpackConfig: any,
   tiga: TigaConfig
 ) {
-  const { devServer, ...rest } = tiga;
-  console.info('devServer', devServer);
+  const {
+    devServer,
+    devtool,
+    publicPath,
+    outputPath,
+    proxy,
+    alias,
+    chainWebpack,
+    ...rest
+  } = tiga;
+
   const mergeConfig = webpackAutoMerge(webpackConfig, {
-    devServer
+    output: {
+      path: outputPath,
+      publicPath
+    },
+    devtool,
+    devServer: {
+      ...devServer,
+      proxy
+    },
+    resolve: {
+      alias
+    }
   });
-  return mergeConfig;
+
+  chainWebpack?.(config);
+
+  const res = webpackAutoMerge(mergeConfig, config?.toConfig() || {});
+  return res;
 }
 
 export const webpackAutoMerge = (webpackConfig: unknown, customConfig: any) => {
